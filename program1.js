1c4192fb294989fceae6ef90bbebd50adb648725
Arjunmachidi <146446642+Arjunmachidi@users.noreply.github.com>
2024-11-11 15:10:03 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -25,27 +25,3 @@ var isValid = function(s) {
 };
 
 module.exports = { isValid };
-
-
-var isValid = function(s) {
-    const stack = [];
-    const matchingBrackets = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
-    
-    for (const char of s) {
-        if (char === '(' || char === '{' || char === '[') {
-            stack.push(char);
-        } else {
-            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
-                return false;
-            }
-        }
-    }
-    
-    return stack.length === 0;
-};
-
-module.exports = { isValid };
